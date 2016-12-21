9758f176fe01e93f968f11676e061019f0c2eb3a
Eleni Mijalis <elenimijalis@gmail.com>
2016-12-21 13:23:43 -0600

    using itertools groupby

--- parse-pepcoil.py
+++ parse-pepcoil.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 import sys
 import argparse
+import itertools
 
 def parse_pepcoil(data):
-    sys.exit()
+    groups = []
+    buff = []
+    for key, group in itertools.groupby(data, lambda line: line.startswith('PEPCOIL of')):
+        if key:
+            if len(buff):
+                groups.append(buff)
+            buff = []
+            buff += list(group)
+        else:
+            buff += list(group)
+    groups.append(buff)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='outputs pepcoil data in tabular format')
     parser.add_argument('pepcoil_data', type=argparse.FileType("r"), help='pepcoil file')
     args = parser.parse_args()
-    gff3_diff(args.pepcoil_data)
+    parse_pepcoil(args.pepcoil_data)
